b854dcc835d183f6638a8e72efa02db5f77661e4
Kerims23 <ks835@njit.edu>
2024-09-21 22:19:13 -0400

    currently broke up the files and added the new method _perform_operations. Now I am going to look to add the test versions for each module

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -42,6 +42,10 @@ Basically instance are literally instances a person can use
 Class are things to check historcal things 
 Static are things to check validation 
 
+
+
+### 
+In part 3 the goal was 
 '''
 # calculator/__init__.py
 from calculator.operations import add, subtract, multiply, divide
@@ -51,32 +55,31 @@ class Calculator:
     '''Calculator class to perform basic operations and track history.'''
 
     @staticmethod
+    def _perform_operation(a: float, b: float, operation) -> float:
+        """Create and perform a calculation, then return the result."""
+        result = operation(a, b)
+        Transactions.add_to_history(operation.__name__, a, b, result)
+        return result
+
+    @staticmethod
     def add(a: float, b: float) -> float:
         '''Performs addition and stores the result in the history.'''
-        result = add(a, b)
-        Transactions.add_to_history('add', a, b, result)
-        return result
+        return Calculator._perform_operation(a, b, add)
 
     @staticmethod
     def subtract(a: float, b: float) -> float:
         '''Performs subtraction and stores the result in the history.'''
-        result = subtract(a, b)
-        Transactions.add_to_history('subtract', a, b, result)
-        return result
+        return Calculator._perform_operation(a, b, subtract)
 
     @staticmethod
     def multiply(a: float, b: float) -> float:
         '''Performs multiplication and stores the result in the history.'''
-        result = multiply(a, b)
-        Transactions.add_to_history('multiply', a, b, result)
-        return result
+        return Calculator._perform_operation(a, b, multiply)
 
     @staticmethod
     def divide(a: float, b: float) -> float:
-        '''Performs division and stores the result in the history.'''
-        result = divide(a, b)
-        Transactions.add_to_history('divide', a, b, result)
-        return result
+        '''Performs division using the operation defined in operations.'''
+        return Calculator._perform_operation(a, b, divide)
 
     @classmethod
     def get_history(cls) -> list:
